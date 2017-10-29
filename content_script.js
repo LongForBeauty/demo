7d2beef023fccece8579b3df77a0ad9e5206cb53
Zilong W <zw1e09@soton.ac.uk>
2017-10-29 14:21:47 +0800

    Solve issue:popup CSS overritten by loaded page + selection across paragraphs is not allowed

--- content_script.js
+++ content_script.js
@@ -1,3 +1,33 @@
+//Create HTML element for the popup box
+var popup = document.createElement('div');
+popup.style.position = "absolute";
+popup.style.padding = '5px 10px';
+popup.style.lineHeight = '2';
+popup.style.backgroundColor = "#CCCCCC";
+popup.style.borderBottomLeftRadius = '12px';
+popup.style.boxShadow = "5px 5px 3px #888888";
+popup.style.zIndex = "99";
+//Create HTML element for the header of the popup box
+var hint = document.createElement('h3');
+hint.style.position = "relative";
+hint.style.top = "5px";
+hint.style.bottom = "2px";
+hint.style.marginBottom = '0px';	
+hint.style.left = "10px";
+hint.style.fontWeight = "800";
+hint.style.letterSpacing = "1.5px";
+hint.style.fontSize = '18px';
+//Create HTML element for the tags inside the popup box
+var tags = document.createElement('p');
+tags.style.position = "relative";
+tags.style.top = '5px';
+tags.style.left = "10px";
+tags.style.fontWeight = "700";
+tags.style.fontSize = '15px';
+
+document.body.appendChild(popup);
+popup.appendChild(hint);
+popup.appendChild(tags);
 
 var rootElement = document.documentElement;
 var firstTier = rootElement.childNodes;
@@ -7,48 +37,32 @@ var popup_height = window.innerHeight * 0.25;	//height of the popup window
 var popup_y_offset = -1.2 * popup_height;  //positive number means moving downwards.
 var popup_x_offset = -0.3 * popup_width;  //positive number means moving towards right.
 
-
-//Create HTML element for the popup box
-var popup = document.createElement('div');
-popup.style.position = "absolute";
-popup.style.backgroundColor = "#CCCCCC";
-popup.style.boxShadow = "5px 5px 3px #888888";
-popup.style.zIndex = "99";
-//Create HTML element for the header of the popup box
-var hint = document.createElement('h3');
-hint.style.position = "relative";
-hint.style.top = "5px";
-hint.style.left = "10px";
-hint.style.fontWeight = "800";
-hint.style.letterSpacing = "1.5px";
-//Create HTML element for the tags inside the popup box
-var tags = document.createElement('p');
-tags.style.position = "relative";
-tags.style.left = "10px";
-tags.style.fontWeight = "700";
-
-document.body.appendChild(popup);
-popup.appendChild(hint);
-popup.appendChild(tags);
-
 	for (var i = 0; i < firstTier.length; i++) {
 		firstTier[i].addEventListener('mouseup', function () {
-			if(window.getSelection().toString().trim() !== ""){
+			var sel = window.getSelection().toString().trim();
+			if(sel !== ""){
 			    var range = window.getSelection().getRangeAt(0);
 		 		var span = document.createElement('span');
+
+		 		//alert("Start container:" + range.startContainer+"  "+"End container:" + range.endContainer);
+		 		//alert("Start:"+range.startOffset+"   "+"end:"+range.endOffset);
 		 		//Display the selected text in a pop up window
-				tags.innerHTML = window.getSelection().toString().trim();
+				tags.innerHTML = sel;
 				hint.innerHTML = "点击并保存到所属类别";
 				//Highlight the selected text
-				span.style.backgroundColor = 'black';
-				span.style.color = 'white'
+				span.style.backgroundColor = 'pink';
+				span.style.color = 'black';
+				span.style.fontWeight = '800';
 
-	    		span.appendChild(range.extractContents());
-	    		range.insertNode(span);  
+	    		//span.appendChild(range.extractContents());
+	    		//range.insertNode(span); 
+	    		range.surroundContents(span);
+	    		//alert("Start container:" + range.startContainer+"  "+"End container:" + range.endContainer);
+	    		//alert("Start:"+range.startOffset+"   "+"end:"+range.endOffset);
 	      		//Get (left and top) co-ordinates of the selected text
 	      		var rect = span.getBoundingClientRect(),
-	      		scrollLeft = window.scrollX || document.documentElement.scrollLeft;
-	      		scrollTop = window.scrollY || document.documentElement.scrollTop;
+	      		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+	      		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 	    		//Put pop-up window up of the selected text
 	    		popup.style.width = popup_width + "px";
 	    		popup.style.height = popup_height + "px";

4ead40fdf7a301a00d35779a44b594275b73166b
LiveToDefine <michaelwang9053@gmail.com>
2017-12-28 05:26:15 -0600

    Add files via upload

---
+++ demo_test/README.md
@@ -0,0 +1,36 @@
+# demo
+### A chrome extension - save what you read into a mindmap 
+
+### What this repository is for?
+This repository is for storing and managing files of a chrome extension demo.
+
+### Why making this chrome extension demo?
+For people who browse and read on the internet, they may want to save pieces of information (text) that they read from the webpage. 
+In common saving tools, such as Evernote, those pieces of information are saved into a notebook, and sometimes with tags. 
+However, those saved information may never be read again by users because they are information fragmentation, not structural, and difficult to be found whenever users need them. 
+Unlike these tools, **this chrome extension intends to save users' interested information and automatically generate a 'mindmap' for structural and visual
+demonstration.** 
+As users browse and read more about the same topic, the mindmap will 'flourish' as well so does the users' knowledge about a topic. 
+Therefore, the knowledge has been kept for the user. 
+
+**This tool is desperately needed** by people who:
+```
+1. regularly intake a lot of text based information (e.g. reports, news); 
+2. they want to digest those information and form a rational judgement. 
+```
+### How does the user interact with this tool after their installations?
+1. Users select text on a web browser(by using a mouse).
+2. A popup window will come out with suggested 'root' tags, which users need to choose by clicking or users can type in their own tags.
+3. Then, the popup window will refresh with suggested 'branch tags', which are closest in logic to the previous selected tag.
+4. If users think the root tag has already very well categorized their selection, then users can click on 'save' button and finish the action. 
+5. Or if they want to choose those 'branch' tags, then they can click on those tags or type in their owns.
+6. Steps 3 - 5 will be continued until users click on 'save' button and finish the action. 
+
+In another interface, users can see their saved information through a mindmap separated by root tags, which they can edit as well. 
+
+### Author: Zilong Wang
+- **Contact:** 1007273820@qq.com
+- **LinkedIn:** http://bit.ly/2vIsjbK
+- **Personal website:** wzl90.com
+
+
--- demo_test/content_script.js
+++ demo_test/content_script.js
@@ -1,151 +1,245 @@
 /*********************************************************************
-/*HTML and CSS part 
+/*HTML and CSS part
 /*********************************************************************/
 //Create HTML element for the popup box
-var popup = document.createElement('div');
-popup.style.position = "absolute";
-popup.style.paddingTop = '5px';
-popup.style.paddingBottom = '5px';
-popup.style.paddingLeft = '10px';
-popup.style.paddingRight = '10px';
+document.onreadystatechange = function () {
+	if (document.readyState === "complete" ) {
+		var popup = document.createElement('div');
+		popup.id = 'popup_container';
 
-popup.style.lineHeight = '2';
-popup.style.backgroundColor = "#CCCCCC";
-popup.style.borderBottomLeftRadius = '12px';
-popup.style.boxShadow = "5px 5px 3px #888888";
-popup.style.zIndex = "999";
-//Create HTML element for the header of the popup box
-var hint = document.createElement('h3');
-hint.style.position = "relative";
-hint.style.marginTop = "5px";
-hint.style.marginBottom = '5px';
-hint.style.marginLeft = "0px";
-hint.style.marginRight = "0px";
-hint.style.paddingTop = "0px";
-hint.style.paddingBottom = "0px";
+		// declare an field text for the input text box
+		var comment_hint = document.createElement('h4');
+		comment_hint.id = 'hint';
+		comment_hint.innerHTML = 'Your comment:';
 
-hint.style.fontWeight = "800";
-hint.style.letterSpacing = "1.5px";
-hint.style.fontSize = '18px';
-//Create HTML element for the tags inside the popup box
-var tags = document.createElement('div');
-tags.style.position = "relative";
-tags.style.marginTop = '5px';
-tags.style.marginBottom = '5px';
-tags.style.marginLeft = "0px";
-tags.style.marginRight = '0px';
-tags.style.paddingTop = '0px';
-tags.style.paddingBottom = '0px';
-tags.style.paddingLeft= '0px';
-tags.style.paddingRight= '0px';
+		// declare an input text box for user comment
+		var comment_input = document.createElement('textarea');
+		comment_input.id = 'user_comment';
+		comment_input.rows = '2';
+		comment_input.placeholder = 'e.g. What is the AI';
+		// declare an field text for the topic
+		var topic_hint = document.createElement('h4');
+		topic_hint.innerHTML = 'Select a topic:'
+		topic_hint.id = 'hint';
 
-tags.style.fontWeight = "700";
-tags.style.fontSize = '15px';
+		// declare an submit button
+		var submit_btn = document.createElement('BUTTON');
+		submit_btn.name = 'submit';
+		submit_btn.className = "myBtn myBtn-dark";
+		$(submit_btn).text("SAVE");
 
-//Create HTML element for a save button
+		//Create HTML element for the tags inside the popup box
+		var tags = document.createElement('div');
+		tags.style.position = "relative";
+		tags.style.marginTop = '5px';
+		tags.style.marginBottom = '5px';
+		tags.style.marginLeft = "0px";
+		tags.style.marginRight = '0px';
+		tags.style.paddingTop = '0px';
+		tags.style.paddingBottom = '0px';
+		tags.style.paddingLeft= '0px';
+		tags.style.paddingRight= '0px';
 
+		tags.style.fontWeight = "700";
+		tags.style.fontSize = '15px';
 
-/*********************************************************************
-/*JavaScript part 
-/*********************************************************************/
-document.body.appendChild(popup);
-popup.appendChild(hint);
-popup.appendChild(tags);
+		/*********************************************************************
+		/*JavaScript part
+		/*********************************************************************/
+		document.body.appendChild(popup);
+		//popup.appendChild(hint);
+		popup.appendChild(comment_hint);
+		popup.appendChild(comment_input);
+		popup.appendChild(topic_hint);
+		popup.appendChild(tags);
+		popup.appendChild(submit_btn);
+		popup.style.display = "none";
 
-var rootElement = document.documentElement;
-var firstTier = rootElement.childNodes;
+		var rootElement = document.documentElement;
+		var firstTier = rootElement.childNodes;
 
-//Event handler for user pressing the keyboard
-document.addEventListener('keydown', (event) => {
-	//when the 'alt' key is pressed
-	if (event.altKey) {
-		for (var i = 0; i < firstTier.length; i++) {
-			//Event handler for 'user selection' on the webpage
-			firstTier[i].addEventListener('mouseup', function () {
-			var sel = window.getSelection().toString().trim();
-			tags.innerHTML = null;
-			if(sel !== "") {
-			    var range = window.getSelection().getRangeAt(0);
-		 		var span = document.createElement('span');
-		 		//Display the selected text in a pop up window
-		 		sel = sel.toUpperCase();
-				var clickTag = sel.split(" ", 10);
-				hint.innerHTML = "点击并保存到所属类别";
-				//Make tags as buttons
-				createBtnNodes(tags, clickTag.length);
-				for (var i = 0; i < clickTag.length; i++) {
-					tags.childNodes[i].innerHTML = clickTag[i];
-					/*********************************************************************
-					/*CSS part 
-					/*********************************************************************/
-					tags.childNodes[i].style.border = "2px ridge grey";
-					tags.childNodes[i].style.marginRight = '2px';
+		var user_selection, user_comment, topic;
+
+		//Event handler for user pressing the keyboard
+		document.addEventListener('keydown', (event) => {
+			//when the 'alt' key is pressed
+			if (event.altKey) {
+				for (var i = 0; i < firstTier.length; i++) {
+					//Event handler for 'user selection' on the webpage
+					firstTier[i].addEventListener('mouseup', function (e) {
+						//Elements in the popup window is set to be not selectable!
+						if (!popup.contains(e.target)) {
+							var sel = window.getSelection().toString().trim();
+							user_selection = sel;
+							//**********************************************************************
+							tags.innerHTML = null;
+							if(sel !== "") {
+								var range = window.getSelection().getRangeAt(0);
+								var span = document.createElement('span');
+								//Display the selected text in a pop up window
+								sel = sel.toUpperCase();
+								var clickTag = sel.split(" ", 8);
+								//hint.innerHTML = "点击并保存到所属类别";
+								//Make tags as buttons
+								createBtnNodes(tags, clickTag.length);
+
+								//Add event listerner to each tag
+								for (var i = 0; i < clickTag.length; i++) {
+									tags.childNodes[i].innerHTML = clickTag[i];
+									/*********************************************************************
+									/*CSS part
+									/*********************************************************************/
+									tags.childNodes[i].style.border = "2px ridge grey";
+									tags.childNodes[i].style.marginRight = '2px';
+								}
+								//Highlight the selected text
+								span.style.backgroundColor = 'pink';
+								span.style.color = 'black';
+								span.style.fontWeight = '800';
+
+								range.surroundContents(span);
+								//Set up popup box's properties: width, height and offsets.
+								var popup_width = window.innerWidth * 0.3;	//width of the popup window
+								var popup_height = window.innerHeight * 0.25;	//height of the popup window
+								var popup_y_offset = -1.2 * popup_height;  //positive number means moving downwards.
+								var popup_x_offset = 0  * popup_width;  //positive number means moving towards right.
+								//Get (left and top) co-ordinates of the selected text
+								var rect = span.getBoundingClientRect(),
+								scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+								scrollTop = window.pageYOffset || document.documentElement.scrollTop,
+								popupLeft = rect.left + scrollLeft + popup_x_offset,
+								popupTop = rect.top + scrollTop + popup_y_offset;
+								//Put pop-up window up of the selected text
+								popup.style.width = popup_width + "px";
+								popup.style.height = popup_height + "px";
+								//If popup window does not across the viewport's right edge
+								if ((rect.left + popup_width) <= window.innerWidth) {
+									popup.style.left = popupLeft + "px" ;
+								}
+								else {
+									popupLeft = window.innerWidth - popup_width + scrollLeft;
+									popup.style.left = popupLeft + "px";
+								}
+								//If popup window does not across the viewport's top edge
+								if ((rect.top - popup_height) >= 0) {
+									popup.style.top = popupTop +  "px" ;
+								}
+								else {
+									popupTop = rect.top + scrollTop + (0.2 * popup_height);
+									popup.style.top = popupTop + 'px';
+
+								}
+
+								popup.style.display = "initial";
+
+								//Event handler for user selecting the topics
+								for (var j = 0; j <= tags.childNodes.length; j++) {
+									tags.childNodes[j].addEventListener('mousedown', function(e){
+										e.target.style.backgroundColor = '#B22222';
+										e.target.style.color = '#FFFFFF';
+
+										if (e.target.type == 'text') {
+											e.target.addEventListener('input', function(){
+												topic = e.target.value;
+											})
+											
+										}
+										else {
+											topic = e.target.innerHTML;
+										}
+										
+									});
+								}
+							}
+							//Only if the user clicks on none-popup DOM element, popup disppeared.
+							else {
+								popup.style.display = "none" ;
+							}
+						}
+					});
+				};
+			};
+		});
+
+		//Event handler for user typing in the comment
+		comment_input.addEventListener('input', function(){
+			user_comment = comment_input.value;
+		});
+
+		//Send user's selection, comments, topic to the backend
+		$(submit_btn).click(function(){
+			$.ajaxSetup({
+				//crossOrigin: true,
+				beforeSend: function(xhr, settings) {
+					if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+						// Send the token to same-origin, relative URLs only.
+						// Send the token only if the method warrants CSRF protection
+						// Using the CSRFToken value acquired earlier
+						xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+					}
 				}
-				//Highlight the selected text
-				span.style.backgroundColor = 'pink';
-				span.style.color = 'black';
-				span.style.fontWeight = '800';
-
-	    		range.surroundContents(span);
-	    		//Set up popup box's properties: width, height and offsets.
-	    		var popup_width = window.innerWidth * 0.3;	//width of the popup window
-				var popup_height = window.innerHeight * 0.25;	//height of the popup window 
-				var popup_y_offset = -1.2 * popup_height;  //positive number means moving downwards.
-				var popup_x_offset = 0  * popup_width;  //positive number means moving towards right.
-	      		//Get (left and top) co-ordinates of the selected text
-	      		var rect = span.getBoundingClientRect(),
-	      		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-	      		scrollTop = window.pageYOffset || document.documentElement.scrollTop,
-	      		popupLeft = rect.left + scrollLeft + popup_x_offset,
-	      		popupTop = rect.top + scrollTop + popup_y_offset;
-	    		//Put pop-up window up of the selected text
-	    		popup.style.width = popup_width + "px";
-	    		popup.style.height = popup_height + "px";
-	    		//If popup window does not across the viewport's right edge
-	    		if ((rect.left + popup_width) <= window.innerWidth) {
-	    			popup.style.left = popupLeft + "px" ;
-	    		}
-	    		else {
-	    			popupLeft = window.innerWidth - popup_width + scrollLeft;
-	    			popup.style.left = popupLeft + "px";
-	    		}
-	    		//If popup window does not across the viewport's top edge	    		
-	    		if ((rect.top - popup_height) >= 0) {
-	    			popup.style.top = popupTop +  "px" ;
-	    		}
-	    		else {
-	    			popupTop = rect.top + scrollTop + (0.2 * popup_height);
-	    			popup.style.top = popupTop + 'px';
-
-	    		}
-	    		
-	    		//When user clicks on the 'tag', the background color of the tag changes
-	    		/*for (var j = 0; j <= tags.childNodes.length; j++) {
-		    		tags.childNodes[j].addEventListener('mousedown', function(e){
-		    			e.target.style.backgroundColor = 'orange';
-		    		});	    			
-	    		}*/
-	    		popup.style.display = "initial";
-	    	}
-	    	else {
-	    		popup.style.display = "none";
-	    	}
-	    	});
-
-		} 
+			});
+			$.post("http://127.0.0.1:8000/mindmap/SaveUserSelection/", {'text_content':user_selection,
+			"user_comment":user_comment,
+			"parent_node":topic,
+		    "csrfmiddleware‌​token":getCookie('csrftoken')},
+			function(){
+				popup.style.display = "none" ;
+			});
+		});
 	}
-});
+}
 
+//**********************************************************************
+//functions
+//**********************************************************************
+//CSRFToken set-up for 'POST' requests
+function csrfSafeMethod(method) {
+	// these HTTP methods do not require CSRF protection
+	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function sameOrigin(url) {
+	// test that a given url is a same-origin URL
+	// url could be relative or scheme relative or absolute
+	var host = document.location.host; // host + port
+	var protocol = document.location.protocol;
+	var sr_origin = '//' + host;
+	var origin = protocol + sr_origin;
+	// Allow absolute or scheme relative URLs to same origin
+	return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+	(url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+	// or any other URL that isn't scheme relative or absolute i.e relative.
+	!(/^(\/\/|http:|https:).*/.test(url));
+}
+
+//Get csrftoken that is stored in cookie
+function getCookie(name) {
+	var cookieValue = null;
+	if (document.cookie && document.cookie != '') {
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i++) {
+			var cookie = jQuery.trim(cookies[i]);
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) == (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
 
 function createBtnNodes(obj, length) {
 	for (var j = 0; j < length; j++) {
 		var newDiv = document.createElement('button');
+		newDiv.className = 'myBtn myBtn-light';
 		obj.appendChild(newDiv);
 	}
+	//The final 'button' is left for user to input their own topic choices
+	var newDiv = document.createElement('input');
+	newDiv.type = 'text';
+	newDiv.placeholder = 'Input your own';
+	newDiv.className = 'myBtn myBtn-light';
+	obj.appendChild(newDiv);
 }
-
-
-
-
-
-
